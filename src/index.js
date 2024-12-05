5f44355c24ecd6aeb6363af290157b17c68f72b8
Vladyslav Shylin <indiggoo228@gmail.com>
2024-12-05 15:01:05 +0200

    code

---
+++ src/index.js
@@ -0,0 +1,27 @@
+/* eslint-disable no-console */
+'use strict';
+
+const express = require('express');
+const cors = require('cors');
+const { errorMiddleware } = require('./middlewares/error.middleware');
+const { messageRouter } = require('./routes/message.route');
+const { roomRouter } = require('./routes/room.route');
+
+require('dotenv/config');
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(errorMiddleware);
+app.use('/message', messageRouter);
+app.use('/room', roomRouter);
+
+app.get('/', (req, res) => {
+  res.send(`PORT:${PORT}`);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
